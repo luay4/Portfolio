7adddf542348e9cd414261b15fa4b84ca34fe048
luay4 <luay0004@stud.kea.dk>
2022-03-03 11:53:22 +0100

    First commit

---
+++ src/main/java/com/example/portfolio/controller/WebController.java
@@ -0,0 +1,23 @@
+package com.example.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/projects")
+    public String projects() {
+        return "projects";
+    }
+}
